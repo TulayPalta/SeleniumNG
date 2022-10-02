1f72e895a22e4b2766a651ac5b6d3aa926edcf96
tulaypalta <paltatulay28@gmail.com>
2022-10-02 12:07:43 +0300

    02.10.2022

--- src/test/java/pages/HmcPage.java
+++ src/test/java/pages/HmcPage.java
@@ -6,19 +6,59 @@ import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
 public class HmcPage {
+
     public HmcPage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
+
+    @FindBy(xpath = "//a[text()='Log in']")
+    public WebElement loginButton;
+
+    @FindBy(xpath = "//input[@id='UserName']")
+    public WebElement usernameTextBox;
+
+    @FindBy(xpath = "//input[@id='Password']")
+    public WebElement passwordTextBox;
+
+    @FindBy(xpath = "//input[@id='btnSubmit']")
+    public WebElement loginPageLoginButton;
+
+    @FindBy(xpath = "//span[@class='username username-hide-on-mobile']")
+    public WebElement managerAfterLogin;
+
+    @FindBy(id = "divMessageResult")
+    public WebElement notLoginWarning;
 
     @FindBy(xpath = "//*[text()='Log in']")
     public WebElement login;
     @FindBy(xpath = "//*[@id='UserName']")
     public WebElement userName;
-
     @FindBy(xpath = "//*[@class='username username-hide-on-mobile']")
     public WebElement girisYapildi;
 
+    @FindBy(xpath = "//a[@id='menuHotels']")
+    public WebElement hotelManagement;
+
+    @FindBy(xpath = "(//a[@href='/admin/RoomReservationAdmin'])[1]")
+    public WebElement roomReservation;
+
+    @FindBy(xpath = "//span[@class='hidden-480']")
+    public WebElement addRoom;
+
     @FindBy(xpath = "//*[text()='Try again please']")
     public WebElement girisYapilamadi;
+
+    @FindBy (xpath= "//select[@id='IDUser']")
+    public WebElement idUser;
+
+    @FindBy (xpath = "//select[@id='IDHotelRoom']")
+    public WebElement hotelRoom;
+
+    @FindBy (xpath= "//div[@class='bootbox-body']")
+    public WebElement roomReservationText;
+
+    @FindBy (xpath = "//button[@data-bb-handler='ok']")
+    public WebElement okButton;
+
 
 }
--- src/test/java/tests/day19/C02_NegatifTest.java
+++ src/test/java/tests/day19/C02_NegatifTest.java
@@ -17,7 +17,7 @@ public class C02_NegatifTest {
         Driver.getDriver().get(ConfigReader.getProperty("hmcUrl"));
         HmcPage hmcPage = new HmcPage();
         //login butonuna bas
-        hmcPage.login.click();
+        hmcPage.loginButton.click();
         //test data username: manager1 ,  test data password : manager1!
         hmcPage.userName.sendKeys(ConfigReader.getProperty("hmcWrongUser"));
         Actions actions = new Actions(Driver.getDriver());
